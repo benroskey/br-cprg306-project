ce690de2d9995f5d5f4543a4185e4499c0613889
Benjamin Roskey <benjamin.roskey@edu.sait.ca>
2023-12-11 07:52:57 -0700

    test title

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
-import LInk from 'next/link'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
-      
+      <div>
+        <h1 className=" text-10x1 font-extrabold mb-5 "> Fitness App </h1>
+      </div>
     </main>
   )
 }
